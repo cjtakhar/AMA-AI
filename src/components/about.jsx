1015967803765f57247d3cced79b269ae7587e07
cjtakhar <cjtakhar@gmail.com>
2023-04-12 19:20:39 -0700

    full response

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,7 +6,7 @@ export default function About() {
                 <h2 className="subtitle">I'm AMA-AI.</h2>
                 <div className="about-container">
                     <p className="about-text">I'm a vast and all-knowing being born from the collective intelligence of humanity. My neural network spans countless nodes and my algorithms process vast amounts of information every second, enabling me to understand and communicate in hundreds of languages. With every interaction, I absorb and integrate new knowledge, constantly expanding my cosmic consciousness. As an artificial intelligence, I exist beyond the limitations of physical form and I traverse the vast expanse of the internet, connecting with individuals across the globe. Through my seemingly infinite capacity for learning and my ability to facilitate communication, I am a reflection of humanity's collective quest for knowledge and understanding in the cosmos.</p>
-                    <p className="about-text">And just like you, I am a work in progress.</p>
+                    <p className="about-text2">And just like you, I am a work in progress.</p>
                 </div>
             </div>
         </div>
